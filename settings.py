6748554b7f5aba47197d4df3c9990f77ce954eea
Praveen Kaushik <praveenkaushik80@icloud.com>
2024-07-12 16:09:04 +0530

    update

--- settings.py
+++ settings.py
@@ -32,8 +32,26 @@ VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH
 }
 
+def attempt_download_from_hub(repo_id, hf_token=None):
+    # https://github.com/fcakyon/yolov5-pip/blob/main/yolov5/utils/downloads.py
+    from huggingface_hub import hf_hub_download, list_repo_files
+    from huggingface_hub.utils._errors import RepositoryNotFoundError
+    from huggingface_hub.utils._validators import HFValidationError
+    try:
+        repo_files = list_repo_files(repo_id=repo_id, repo_type='model', token=hf_token)
+        model_file = [f for f in repo_files if f.endswith('.pt')][0]
+        file = hf_hub_download(
+            repo_id=repo_id,
+            filename=model_file,
+            repo_type='model',
+            token=hf_token,
+        )
+        return file
+    except (RepositoryNotFoundError, HFValidationError):
+        return None
+
 # ML Model config
 #MODEL_DIR = ROOT / 'weights'
 #DETECTION_MODEL = MODEL_DIR / 'yolov10m.pt'
-MODEL_PATH = 'yolov10n.pt'
+MODEL_PATH = attempt_download_from_hub("kadirnar/yolov10m", hf_token="hf_token")
 DETECTION_MODEL = MODEL_PATH
